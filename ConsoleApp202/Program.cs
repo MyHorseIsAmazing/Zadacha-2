a6c66ce145d6092176263ee29abc105367831d4a
myhorseisamazing <leonellus.skin@gmail.com>
2019-05-25 23:23:13 +0300

    Add project files.

---
+++ ConsoleApp202/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp202
+{
+    class Program
+    {
+        static bool skobi(string n)
+        {
+            int duljinata = n.Length;
+            if (duljinata % 2 == 1)
+
+            {
+                return false;
+            }
+            else
+            {
+                if (n.Substring(0, 1) != "(")
+
+                {
+                    return false;
+                }
+                else if (n.Substring(duljinata - 1, 1) != ")")
+
+                {
+                    return false;
+                }
+                else
+                {
+                    int broi = 0;
+                    char skoba = '(';
+                    foreach (char c in n)
+                    {
+                        if (c == skoba)
+                        {
+                            broi++;
+                        }
+                    }
+                    int broi2 = 0;
+                    skoba = ')';
+                    foreach (char c in n)
+                    {
+                        if (c == skoba)
+                        {
+                            broi2++;
+                        }
+                    }
+                    if (broi == broi2)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vuvedi izrechenie sus skobi: ");
+            string skobite = Console.ReadLine();
+            Console.WriteLine(skobi(skobite));
+            Console.ReadLine();
+        }
+    }
+}
